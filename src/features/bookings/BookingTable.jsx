b5a83101ff11ae99b0f4684a1c77219d026a713c
Hwezah <hwezah.dev@gmail.com>
2025-06-02 16:56:55 +0300

    fixed minor bookings is undefined error in BookingTable

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -7,9 +7,12 @@ import Pagination from "../../ui/Pagination";
 // import Menus from "../../ui/Menus";
 
 function BookingTable() {
-  const { bookings, isLoading, count } = useBookings();
+  const { bookings, isLoading, count, error } = useBookings();
   if (isLoading) return <Spinner />;
-  if (!bookings.length) return <Empty resourceName="bookings" />;
+  if (error)
+    return <Empty resourceName={`bookings (Error: ${error.message})`} />;
+  if (!bookings || bookings.length === 0)
+    return <Empty resourceName="bookings" />;
   return (
     // <Menus>
     <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
